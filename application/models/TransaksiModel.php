86d5fe44a074c2ed498aaff0b678913a79c9886f
Naufal Yudhistira <yudhistn@gmail.com>
2019-12-14 12:22:47 +0700

    Make Transaction response more readable

--- application/models/TransaksiModel.php
+++ application/models/TransaksiModel.php
@@ -7,8 +7,16 @@
         public function getTransaksi($id = null)
         {
             if ($id == null) {
+                $this->db->select('transaksi.*, kamar.no_kamar, layanan.nama as layanan, user.nama');
+                $this->db->join('kamar', 'transaksi.id_kamar = kamar.id_kamar');
+                $this->db->join('layanan', 'transaksi.id_layanan = layanan.id_layanan');
+                $this->db->join('user', 'transaksi.id_user = user.id_user');
                 return $this->db->get('transaksi')->result();
             } else {
+                $this->db->select('transaksi.*, kamar.no_kamar, layanan.nama as layanan, user.nama');
+                $this->db->join('kamar', 'transaksi.id_kamar = kamar.id_kamar');
+                $this->db->join('layanan', 'transaksi.id_layanan = layanan.id_layanan');
+                $this->db->join('user', 'transaksi.id_user = user.id_user');
                 return $this->db->get_where('transaksi', ['id_transaksi' => $id])->result();
             }
         }
