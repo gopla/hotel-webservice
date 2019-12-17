dac84c07b86c05e8ceef27e32e95f04773b0b228
Naufal Yudhistira <yudhistn@gmail.com>
2019-12-17 08:39:51 +0700

    Hosting database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'web_hotel',
+	'hostname' => 'sql12.freesqldatabase.com',
+	'username' => 'sql12316002',
+	'password' => 'Hydbi9A3VN',
+	'database' => 'sql12316002',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
--- web_hotel.sql
+++ web_hotel.sql
@@ -23,39 +23,28 @@ CREATE TABLE IF NOT EXISTS `kamar` (
   `tipe` varchar(50) DEFAULT NULL,
   `harga` int(11) DEFAULT NULL,
   `jml_ranjang` int(11) DEFAULT '1',
+  `deskripsi` text,
   `status` int(11) DEFAULT '1',
   `gambar` longtext,
   PRIMARY KEY (`id_kamar`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
--- Dumping data for table web_hotel.kamar: ~4 rows (approximately)
+-- Dumping data for table web_hotel.kamar: ~7 rows (approximately)
 /*!40000 ALTER TABLE `kamar` DISABLE KEYS */;
-INSERT INTO `kamar` (`id_kamar`, `no_kamar`, `tipe`, `harga`, `jml_ranjang`, `status`, `gambar`) VALUES
-	(1, 101, 'Regular', 150000, 1, 1, './hotel-webservice/uploads/kamar/images.jpg'),
-	(23, 102, 'Regular', 250000, 1, 0, './hotel-webservice/uploads/kamar/'),
-	(24, 102, 'Regular', 200000, 1, 1, './hotel-webservice/uploads/kamar/');
+INSERT INTO `kamar` (`id_kamar`, `no_kamar`, `tipe`, `harga`, `jml_ranjang`, `deskripsi`, `status`, `gambar`) VALUES
+	(1, 101, 'Regular', 150000, 1, NULL, 1, './hotel-webservice/uploads/kamar/'),
+	(23, 102, 'Regular', 250000, 1, NULL, 0, './hotel-webservice/uploads/kamar/'),
+	(24, 102, 'Regular', 200000, 1, NULL, 1, './hotel-webservice/uploads/kamar/'),
+	(25, 201, 'Apik', 300000, 1, NULL, 1, './hotel-webservice/uploads/kamar/'),
+	(26, 202, 'Apik', 300000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './hotel-webservice/uploads/kamar/'),
+	(27, 202, 'Apik', 300000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './hotel-webservice/uploads/kamar/'),
+	(28, 202, 'Apik', 300000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './hotel-webservice/uploads/kamar/images2.jpg');
 /*!40000 ALTER TABLE `kamar` ENABLE KEYS */;
-
--- Dumping structure for table web_hotel.layanan
-CREATE TABLE IF NOT EXISTS `layanan` (
-  `id_layanan` int(11) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(100) DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_layanan`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
--- Dumping data for table web_hotel.layanan: ~2 rows (approximately)
-/*!40000 ALTER TABLE `layanan` DISABLE KEYS */;
-INSERT INTO `layanan` (`id_layanan`, `nama`, `harga`) VALUES
-	(1, 'Laundry', 5000),
-	(2, 'Kolam', 15000);
-/*!40000 ALTER TABLE `layanan` ENABLE KEYS */;
 
 -- Dumping structure for table web_hotel.transaksi
 CREATE TABLE IF NOT EXISTS `transaksi` (
   `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,
   `id_kamar` int(11) DEFAULT NULL,
-  `id_layanan` int(11) DEFAULT NULL,
   `id_user` int(11) DEFAULT NULL,
   `tanggal` datetime DEFAULT CURRENT_TIMESTAMP,
   `checkin` datetime DEFAULT NULL,
@@ -63,19 +52,17 @@ CREATE TABLE IF NOT EXISTS `transaksi` (
   `total` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_transaksi`),
   KEY `FK_transaksi_kamar` (`id_kamar`),
-  KEY `FK_transaksi_layanan` (`id_layanan`),
   KEY `FK_transaksi_user` (`id_user`),
   CONSTRAINT `FK_transaksi_kamar` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_transaksi_layanan` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id_layanan`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_transaksi_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table web_hotel.transaksi: ~2 rows (approximately)
+-- Dumping data for table web_hotel.transaksi: ~3 rows (approximately)
 /*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
-INSERT INTO `transaksi` (`id_transaksi`, `id_kamar`, `id_layanan`, `id_user`, `tanggal`, `checkin`, `checkout`, `total`) VALUES
-	(1, 1, 2, 1, '2019-12-13 15:34:29', '2019-12-13 15:34:30', '2019-12-13 15:34:31', 150000),
-	(2, 1, 1, 1, '2019-12-13 15:35:32', '2019-12-13 00:00:00', '2019-12-13 00:00:00', 200000),
-	(5, 23, 2, 1, '2019-12-14 21:08:55', '2019-12-14 00:00:00', '2019-12-14 00:00:00', 265000);
+INSERT INTO `transaksi` (`id_transaksi`, `id_kamar`, `id_user`, `tanggal`, `checkin`, `checkout`, `total`) VALUES
+	(1, 1, 1, '2019-12-13 15:34:29', '2019-12-13 15:34:30', '2019-12-13 15:34:31', 150000),
+	(2, 1, 1, '2019-12-13 15:35:32', '2019-12-13 00:00:00', '2019-12-13 00:00:00', 200000),
+	(5, 23, 1, '2019-12-14 21:08:55', '2019-12-14 00:00:00', '2019-12-14 00:00:00', 265000);
 /*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
 
 -- Dumping structure for table web_hotel.user
@@ -89,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table web_hotel.user: ~0 rows (approximately)
+-- Dumping data for table web_hotel.user: ~1 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id_user`, `nama`, `email`, `username`, `password`, `role`) VALUES
 	(1, 'Marsina', 'a@a.com', 'rizna', '123', 'Admin');
