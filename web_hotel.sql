cd67e3d4ac2d3d365070d096e8146e03129c3d77
Naufal Yudhistira <yudhistn@gmail.com>
2019-12-20 20:02:25 +0700

    Update database

--- web_hotel.sql
+++ web_hotel.sql
@@ -23,22 +23,32 @@ CREATE TABLE IF NOT EXISTS `kamar` (
   `tipe` varchar(50) DEFAULT NULL,
   `harga` int(11) DEFAULT NULL,
   `jml_ranjang` int(11) DEFAULT '1',
+  `lokasi` text,
   `deskripsi` text,
   `status` int(11) DEFAULT '1',
   `gambar` longtext,
   PRIMARY KEY (`id_kamar`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
 
--- Dumping data for table web_hotel.kamar: ~7 rows (approximately)
+-- Dumping data for table web_hotel.kamar: ~16 rows (approximately)
 /*!40000 ALTER TABLE `kamar` DISABLE KEYS */;
-INSERT INTO `kamar` (`id_kamar`, `no_kamar`, `tipe`, `harga`, `jml_ranjang`, `deskripsi`, `status`, `gambar`) VALUES
-	(1, 101, 'Regular', 150000, 1, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './rest-hotel/frontend-hotel/uploads/kamar/images.jpg'),
-	(23, 102, 'Regular', 250000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './rest-hotel/frontend-hotel/hotel-webservice/uploads/kamar/images.jpg'),
-	(24, 103, 'Regular', 200000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './rest-hotel/frontend-hotel/hotel-webservice/uploads/kamar/images.jpg'),
-	(25, 201, 'Apik', 300000, 3, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './rest-hotel/frontend-hotel/hotel-webservice/uploads/kamar/images2.jpg'),
-	(26, 202, 'Apik', 300000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './rest-hotel/frontend-hotel/hotel-webservice/uploads/kamar/images.jpg'),
-	(27, 203, 'Apik', 300000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './rest-hotel/frontend-hotel/hotel-webservice/uploads/kamar/images2.jpg'),
-	(28, 204, 'Apik', 300000, 2, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, './rest-hotel/frontend-hotel/hotel-webservice/uploads/kamar/images2.jpg');
+INSERT INTO `kamar` (`id_kamar`, `no_kamar`, `tipe`, `harga`, `jml_ranjang`, `lokasi`, `deskripsi`, `status`, `gambar`) VALUES
+	(1, 101, 'Regular', 150000, 1, 'JL. Mawar, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images2.jpg'),
+	(23, 102, 'Regular', 250000, 2, 'JL. Mawar, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images2.jpg'),
+	(24, 103, 'Regular', 200000, 2, 'JL. Melati, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images2.jpg'),
+	(25, 201, 'Deluxe', 300000, 2, 'JL. Mawar, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images2.jpg'),
+	(26, 202, 'Deluxe', 300000, 2, 'JL. Mawar, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images2.jpg'),
+	(27, 203, 'Deluxe', 300000, 2, 'JL. Melati, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images2.jpg'),
+	(28, 204, 'Deluxe', 300000, 2, 'JL. Melati, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images2.jpg'),
+	(29, 107, 'Regular', 150000, 1, 'JL. Melati, Malang', 'halo', 1, '/uploads/kamar/images2.jpg'),
+	(30, 108, 'Regular', 150000, 1, 'JL. Melati, Malang', 'halo', 0, '/uploads/kamar/images2.jpg'),
+	(32, 109, 'Regular', 150000, 1, 'JL. Ijen, Malang', 'halo', 1, '/uploads/kamar/images2.jpg'),
+	(37, 301, 'Royal', 500000, 3, 'JL. Melati, Malang', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quae, quibusdam aut minus ex consequatur eaque vel veritatis! Perferendis tempore velit reiciendis illo distinctio inventore cum culpa dolores molestias rerum?', 1, '/uploads/kamar/images3.jpg'),
+	(38, 302, 'Royal', 500000, 3, 'JL. Melati, Malang', 'Larang', 1, '/uploads/kamar/images3.jpg'),
+	(39, 303, 'Royal', 500000, 3, 'JL. Ijen, Malang', 'Larang', 1, '/uploads/kamar/images3.jpg'),
+	(40, 304, 'Royal', 500000, 3, 'JL. Ijen, Kepanjen', 'alsk', 1, '/uploads/kamar/images3.jpg'),
+	(41, 305, 'Royal', 500000, 3, 'JL. Ijen, Malang', 'Lala', 1, '/uploads/kamar/images3.jpg'),
+	(42, 306, 'Royal', 500000, 3, 'JL. Ijen, Malang', 'Lala', 1, '/uploads/kamar/images3.jpg');
 /*!40000 ALTER TABLE `kamar` ENABLE KEYS */;
 
 -- Dumping structure for table web_hotel.transaksi
@@ -55,14 +65,15 @@ CREATE TABLE IF NOT EXISTS `transaksi` (
   KEY `FK_transaksi_user` (`id_user`),
   CONSTRAINT `FK_transaksi_kamar` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_transaksi_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table web_hotel.transaksi: ~3 rows (approximately)
+-- Dumping data for table web_hotel.transaksi: ~4 rows (approximately)
 /*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
 INSERT INTO `transaksi` (`id_transaksi`, `id_kamar`, `id_user`, `tanggal`, `checkin`, `checkout`, `total`) VALUES
 	(1, 1, 1, '2019-12-13 15:34:29', '2019-12-13 15:34:30', '2019-12-13 15:34:31', 150000),
 	(2, 1, 1, '2019-12-13 15:35:32', '2019-12-13 00:00:00', '2019-12-13 00:00:00', 200000),
-	(5, 23, 1, '2019-12-14 21:08:55', '2019-12-14 00:00:00', '2019-12-14 00:00:00', 265000);
+	(5, 23, 1, '2019-12-14 21:08:55', '2019-12-14 00:00:00', '2019-12-14 00:00:00', 265000),
+	(6, 30, 2, '2019-12-20 00:00:00', '2019-12-28 00:00:00', '2019-12-31 00:00:00', 150000);
 /*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
 
 -- Dumping structure for table web_hotel.user
@@ -70,16 +81,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(100) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
+  `no_telp` varchar(15) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `role` enum('Admin','User') DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table web_hotel.user: ~1 rows (approximately)
+-- Dumping data for table web_hotel.user: ~2 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id_user`, `nama`, `email`, `username`, `password`, `role`) VALUES
-	(1, 'Marsina', 'a@a.com', 'rizna', '123', 'Admin');
+INSERT INTO `user` (`id_user`, `nama`, `email`, `no_telp`, `username`, `password`, `role`) VALUES
+	(1, 'Marsina', 'a@a.com', NULL, 'rizna', '123', 'Admin'),
+	(2, 'Joker', 'a@a.com', NULL, 'joker', '123', 'User');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
